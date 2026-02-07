65ba3880c634704d00d3f8c17dbfcf2d75a2653a
davide.gesino <davide_gesino@swissre.com>
2026-02-07 16:15:04 +0100

    change files

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"log"
 	"os"
 
-	"github.com/jesinity/terraform-provider-sigil/internal/provider"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/jesinity/terraform-provider-sigil/internal/provider"
 )
+
+var version = "dev"
 
 func main() {
 	opts := providerserver.ServeOpts{
 		Address: "registry.terraform.io/jesinity/sigil",
 	}
 
-	if err := providerserver.Serve(context.Background(), provider.New("dev"), opts); err != nil {
+	if err := providerserver.Serve(context.Background(), provider.New(version), opts); err != nil {
 		log.New(os.Stderr, "", log.LstdFlags).Printf("provider serve error: %s", err)
 		os.Exit(1)
 	}
